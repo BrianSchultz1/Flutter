98cb1f3e5cde7891cb9234f9b997ec1b529e35d5
Brian Schultz <68474136+BrianSchultz1@users.noreply.github.com>
2022-09-12 15:52:20 -0300

    Add files via upload

---
+++ resultado.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+
+class Resultado extends StatelessWidget {
+  final int pontuacao;
+  final void Function() quandoReiniciarQuestionario;
+  Resultado(this.pontuacao, this.quandoReiniciarQuestionario);
+
+  String get fraseResultado {
+    if (pontuacao < 8) {
+      return 'Parabéns!';
+    } else if (pontuacao < 12) {
+      return 'Você é bom!';
+    } else if (pontuacao < 16) {
+      return 'Impressionante!';
+    } else {
+      return 'Nível Jedi!';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Center(
+          child: Text(
+            'Parabéns!',
+            style: TextStyle(fontSize: 28),
+          ),
+        ),
+        FlatButton(
+          onPressed: quandoReiniciarQuestionario,
+          child: Text(
+            'reiniciar?',
+            style: TextStyle(fontSize: 18),
+          ),
+          textColor: Colors.blue,
+        )
+      ],
+    );
+  }
+}
