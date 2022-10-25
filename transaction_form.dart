ed7561f2b7d69e146aa711d54db0b917694a07fe
Brian Schultz <68474136+BrianSchultz1@users.noreply.github.com>
2022-10-25 17:39:43 -0300

    Add files via upload

---
+++ transaction_form.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+
+class TransactionForm extends StatelessWidget {
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: titleController,
+              decoration: InputDecoration(
+                labelText: 'Nome',
+              ),
+            ),
+            TextField(
+              controller: valueController,
+              decoration: InputDecoration(
+                labelText: 'Valor (R\$)',
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextButton(
+                  child: Text('Nova Despesa'),
+                  // color: Colors.066443,
+                  // textColor: Colors.F3E6E6,
+                  onPressed: () {
+                    print(titleController.text);
+                    print(valueController.text);
+                  },
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
