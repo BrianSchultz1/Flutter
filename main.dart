e53b3849e460344e64f5b802163152bdeb9cb65c
Brian Schultz <68474136+BrianSchultz1@users.noreply.github.com>
2022-10-25 17:38:23 -0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,18 +1,54 @@
+import 'package:aplicativo/components/transaction_list.dart';
+import 'models/transaction.dart';
 import 'package:flutter/material.dart';
 
-main() => runApp(new PerguntaApp());
+main() => runApp(ExpensesApp());
 
-class PerguntaApp extends StatelessWidget {
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyhomePage());
+  }
+}
+
+class MyhomePage extends StatelessWidget {
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+
+  final _transaction = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Perguntas'),
-        ),
-        body: Text('Olá Flutter'),
-        )
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            child: Card(
+              color: Colors.blue,
+              child: Text('Gráfico'),
+              elevation: 5,
+            ),
+          ),
+          TransactionList(_transaction),
+        ],
+      ),
     );
   }
 }
