18e064e63cdeb9ddaf6b82d25d31155e37d69adf
Brian Schultz <68474136+BrianSchultz1@users.noreply.github.com>
2022-11-01 15:33:09 -0300

    Add files via upload

---
+++ chart_bar.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  final String label;
+  final double value;
+  final double percentage;
+
+  ChartBar({
+    required this.label,
+    required this.value,
+    required this.percentage,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        FittedBox(child: Text('${value.toStringAsFixed(2)}')),
+        SizedBox(height: 5),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1.0,
+                  ),
+                  color: Color.fromRGBO(136, 136, 136, 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: percentage,
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+              )
+            ],
+          ),
+        ),
+        SizedBox(height: 5),
+        Text(label),
+      ],
+    );
+  }
+}
